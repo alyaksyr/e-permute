e2cc1a057afce35b61c5a2977b60113e98ce1eab
alyaksyr <alyaksyr3516@gmail.com>
2021-11-16 02:44:15 +0000

    Model User update

--- src/app/modeles/user.ts
+++ src/app/modeles/user.ts
@@ -1,2 +1,26 @@
 export class User {
+    id:number;
+    mobile:string;
+    email:string;
+    role:string;
+    password:string;
+    token:string
+
+    constructor(options:
+        {
+            id?:number,
+            role?:string,
+            mobile?:string,
+            email?:string,
+            password?:string
+            token?:string
+        }
+    ={}){
+        this.id = options.id;
+        this.mobile = options.mobile;
+        this.role = options.role;
+        this.email = options.email;
+        this.password = options.password;
+        this.token = options.token;
+    }
 }
